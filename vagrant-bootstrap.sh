88938746bb16f25b3a8ca5dc71d8fcabb2afb2b1
Josh Atkins <josh.atkins.dev@gmail.com>
2017-03-20 04:50:48 +0000

    Install docker

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -5,10 +5,25 @@ apt-get install -y fortune git software-properties-common
 apt-get update
 
 echo "Installing Ansible..."
-apt-get install -y ansible
+apt-get install -y ansible 
 
 ansible-galaxy install kbrebanov.openvpn
 # ansible-galaxy install angstwad.docker_ubuntu
+
+
+echo "Installing Docker..."
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint | grep "4096R/F273FCD8 2017-02-22" || exit
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+groupadd docker
+usermod -aG docker vagrant
+systemctl enable docker
 
 wget -O - https://bootstrap.pypa.io/get-pip.py | python
 pip install boto virtualenv awscli
