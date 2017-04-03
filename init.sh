1e7ea6d9fb7194976b0aa5bcddbb652ba3fabbfc
Josh Atkins <josh.atkins.dev@gmail.com>
2017-04-03 17:26:41 +0100

    add aliases

---
+++ init.sh
@@ -0,0 +1,15 @@
+
+alias e="exit"
+alias i=". init.sh"
+
+
+alias g="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias gr="git reflog"
+alias gl="git log"
+alias go="git log --oneline"
+alias gv="git log --graph --decorate --oneline --all"
+alias gk="git checkout"
+alias gb="git branch"
