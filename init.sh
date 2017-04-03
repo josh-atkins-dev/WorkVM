d9516a23220981249c23a641bbdb23dae50637fe
Josh Atkins <josh.atkins.dev@gmail.com>
2017-04-03 17:54:28 +0100

    customise colours

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+export TERM=xterm  
 
 alias e="exit"
 alias i=". init.sh"
@@ -13,3 +14,193 @@ alias go="git log --oneline"
 alias gv="git log --graph --decorate --oneline --all"
 alias gk="git checkout"
 alias gb="git branch"
+
+
+
+#-------------------------------------------------------------
+# Greeting, motd etc. ...
+#-------------------------------------------------------------
+
+# Color definitions (taken from Color Bash Prompt HowTo).
+# Some colors might look different of some terminals.
+# For example, I see 'Bold Red' as 'orange' on my screen,
+# hence the 'Green' 'BRed' 'Red' sequence I often use in my prompt.
+
+
+# Normal Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+NC="\033[m"               # Color Reset
+
+
+ALERT=${BWhite}${On_Red} # Bold White on red background
+
+
+
+
+function update_public_ip () {
+    export PUBLIC_IP=$(wget -qO- -t 1 --timeout 5 http://ipecho.net/plain ; echo)    
+}
+update_public_ip
+
+
+echo -e "${BCyan}This is BASH ${BRed}${BASH_VERSION%.*}${NC}\n${BCyan}Public IP: ${BRed}${PUBLIC_IP}${NC}\n"
+date
+
+echo -e "\n"
+if [ -x /usr/local/bin/fortune ]; then
+    /usr/local/bin/fortune -s     # Makes our day a bit more fun.... :-)
+fi
+echo -e "\n"
+
+function _exit()              # Function to run upon exit of shell.
+{
+    echo -e "${BRed}Hasta la vista, baby${NC}"
+}
+trap _exit EXIT
+
+
+
+
+
+#-------------------------------------------------------------
+# Shell Prompt - for many examples, see:
+#       http://www.debian-administration.org/articles/205
+#       http://www.askapache.com/linux/bash-power-prompt.html
+#       http://tldp.org/HOWTO/Bash-Prompt-HOWTO
+#       https://github.com/nojhan/liquidprompt
+#-------------------------------------------------------------
+# Current Format: [TIME USER@HOST PWD] >
+# TIME:
+#    Green     == machine load is low
+#    Orange    == machine load is medium
+#    Red       == machine load is high
+#    ALERT     == machine load is very high
+# USER:
+#    Cyan      == normal user
+#    Orange    == SU to user
+#    Red       == root
+# HOST:
+#    Cyan      == local session
+#    Green     == secured remote connection (via ssh)
+#    Red       == unsecured remote connection
+# PWD:
+#    Green     == more than 10% free disk space
+#    Orange    == less than 10% free disk space
+#    ALERT     == less than 5% free disk space
+#    Red       == current user does not have write privileges
+#    Cyan      == current filesystem is size zero (like /proc)
+# >:
+#    White     == no background or suspended jobs in this shell
+#    Cyan      == at least one background job in this shell
+#    Orange    == at least one suspended job in this shell
+#
+#    Command is added to the history file each time you hit enter,
+#    so it's available to all shells (using 'history -a').
+
+
+# Test connection type:
+if [ -n "${SSH_CONNECTION}" ]; then
+    CNX=${Green}        # Connected on remote machine, via ssh (good).
+elif [[ "${DISPLAY%%:0*}" != "" ]]; then
+    CNX=${ALERT}        # Connected on remote machine, not via ssh (bad).
+else
+    CNX=${Purple}        # Connected on local machine.
+fi
+
+# Test user type:
+if [[ ${USER} == "root" ]]; then
+    SU=${Red}           # User is root.
+elif [[ ${USER} != $(logname) ]]; then
+    SU=${BRed}          # User is not login user.
+else
+    SU=${BPurple}         # User is normal (well ... most of us are).
+fi
+
+
+
+# Returns a color indicating system load.
+function load_color()
+{
+  echo -en ${Green}
+}
+
+# Returns a color according to free disk space in $PWD.
+function disk_color()
+{
+  echo -en ${BGreen}
+}
+
+# Returns a color according to running/suspended jobs.
+function job_color()
+{
+  echo -en ${BCyan}
+}
+
+# Adds some text in the terminal frame (if applicable).
+
+
+# Now we construct the prompt.
+PROMPT_COMMAND="history -a"
+case ${TERM} in
+  *term | rxvt | linux)
+        PS1="\[\$(load_color)\][\A\[${NC}\] "
+        # Time of day (with load info):
+        PS1="\[\$(load_color)\][\A\[${NC}\] "
+        # User@Host (with connection type info):
+        PS1=${PS1}"\[${SU}\]\u\[${NC}\]@\[${CNX}\]\h\[${NC}\] "
+        # PWD (with 'disk space' info):
+        PS1=${PS1}"\[\$(disk_color)\]\W]\[${NC}\] "
+        # Prompt (with 'job' info):
+        PS1=${PS1}"\[\$(job_color)\]>\[${NC}\] "
+        # Set title of current xterm:
+        PS1=${PS1}"\[\e]0;[\u@\h] \w\a\]"
+        ;;
+    *)
+        PS1="(\A \u@\h \W) > " # --> PS1="(\A \u@\h \w) > "
+                               # --> Shows full pathname of current dir.
+        ;;
+esac
+
+
+
+export TIMEFORMAT=$'\nreal %3R\tuser %3U\tsys %3S\tpcpu %P\n'
+export HISTIGNORE="&:bg:fg:ll:h"
+export HISTTIMEFORMAT="$(echo -e ${BCyan})[%d/%m %H:%M:%S]$(echo -e ${NC}) "
+export HISTCONTROL=ignoredups
+export HOSTFILE=$HOME/.hosts    # Put a list of remote hosts in ~/.hosts
+
+
+
+
+
+
+
+
